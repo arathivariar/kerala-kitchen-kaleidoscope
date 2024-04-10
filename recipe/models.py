aa08b3bb7bc1e1eaf83248028bd239b56189a85c
Arathi Variar <arathivariar@gmail.com>
2024-04-10 15:57:35 +0000

    Created the Recipe model and migrated to the database

--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Recipe(models.Model):
+    recipe_id = models.IntegerField()
+    title = models.CharField(max_length=200, unique=True)
+    description = models.SlugField(max_length=200, unique=True)
+    chef = models.ForeignKey(User, on_delete=models.CASCADE, related_name="kitchen_recipes")
+    ingredients = models.TextField()
+    method = models.TextField()
+    dish_type = models.CharField(max_length=200)
+    prep_time = models.CharField(max_length=200)
+    posted_on = models.DateTimeField(auto_now_add=True)
