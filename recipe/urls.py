c73dcf325b6944484fdd565db89375600ae9816e
Arathi Variar <arathivariar@gmail.com>
2024-04-13 13:27:26 +0000

    Updated url pattern for recipe detail for testing

--- recipe/urls.py
+++ recipe/urls.py
@@ -1,7 +1,7 @@
-from . import views
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path("", views.RecipeList.as_view(), name='home'),
-    path('<description:description>/', views.recipe_detail, name='recipe_detail'),
+    path('', views.RecipeList.as_view(), name='home'),
+    path('', views.recipe_detail, name='recipe_detail'),
 ]
