4149d7cf6b5490cfa38d29052f1465d18e53c8b5
Arathi Variar <arathivariar@gmail.com>
2024-04-13 17:44:16 +0000

    Updated the url pattern for recipe detail view

--- recipe/urls.py
+++ recipe/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.RecipeList.as_view(), name='home'),
-    path('', views.recipe_detail, name='recipe_detail'),
+    path('<description:description>/', views.recipe_detail, name='recipe_detail'),
 ]
