57e71e794e14cfa3d668af73ea9d7eb3b48f7274
Arathi Variar <arathivariar@gmail.com>
2024-04-14 12:57:52 +0000

    Updated admin file to add doc string

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,14 +1,19 @@
 from django.contrib import admin
-from .models import Recipe, Comment
 from django_summernote.admin import SummernoteModelAdmin
+from .models import Recipe, Comment
 
 
 @admin.register(Recipe)
 class PostAdmin(SummernoteModelAdmin):
+    
+    """
+    Lists fields for display in admin, fileds for search,
+    field filters, fields to prepopulate and rich-text editor.
+    """
 
     list_display = ('title', 'description', 'dish_type')
     search_fields = ['title', 'ingredients']
-    list_filter = ('dish_type','posted_on')
+    list_filter = ('dish_type','posted_on',)
     prepopulated_fields = {'description': ('title',)}
     summernote_fields = ('method',)
 
